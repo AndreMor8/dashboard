8514cc4ea546163384e0e5a7931e27de2c1b2a96
AndreMor955 <moralesandre75@gmail.com>
2020-10-22 09:57:16 -0500

    Hm.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require("./database.js");
 const express = require("express");
 const passport = require("passport");
 const csrf = require('csurf');
-global.antixss = function(string = "") {
+global.antixss = function (string = "") {
   string = string.replace('&', '&amp;');
   string = string.replace('<', '&lt;');
   string = string.replace('>', '&gt;');
@@ -17,7 +17,7 @@ global.antixss = function(string = "") {
   string = string.replace("'", '&#x27;');
   return string;
 };
-global.antixsslinks = function(string = "") {
+global.antixsslinks = function (string = "") {
   string = string.replace('<', '&lt;');
   string = string.replace('>', '&gt;');
   string = string.replace('"', '&quot;');
@@ -25,7 +25,7 @@ global.antixsslinks = function(string = "") {
   return string;
 };
 (async () => {
-  if(process.argv[2] !== "ci") await db.then(() => console.log("Connected to the database"));
+  if (process.argv[2] !== "ci") await db.then(() => console.log("Connected to the database"));
   const app = express();
   const session = require("express-session");
   const MongoStore = require("connect-mongo")(session);
@@ -33,18 +33,32 @@ global.antixsslinks = function(string = "") {
   app.use(express.static(__dirname + "/public"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }))
-  app.use(
-    session({
-      secret: process.env.SECRET || "?",
-      cookie: {
-        maxAge: 60000 * 60 * 24
-      },
-      saveUninitialized: false,
-      resave: false,
-      name: "discord.oauth2",
-      store: new MongoStore({ mongooseConnection: require("mongoose").connection })
-    })
-  );
+  if (process.argv[2] === "ci") {
+    app.use(
+      session({
+        secret: process.env.SECRET || "?",
+        cookie: {
+          maxAge: 60000 * 60 * 24
+        },
+        saveUninitialized: false,
+        resave: false,
+        name: "discord.oauth2"
+      })
+    );
+  } else {
+    app.use(
+      session({
+        secret: process.env.SECRET || "?",
+        cookie: {
+          maxAge: 60000 * 60 * 24
+        },
+        saveUninitialized: false,
+        resave: false,
+        name: "discord.oauth2",
+        store: new MongoStore({ mongooseConnection: require("mongoose").connection })
+      })
+    );
+  }
   app.set("view engine", "ejs");
   app.set("views", require("path").join(__dirname, "views"));
   app.use(passport.initialize());
@@ -54,7 +68,7 @@ global.antixsslinks = function(string = "") {
   app.use("/dashboard", require("./routes/dashboard"));
   app.use("/wwd", require("./routes/wwd"));
   app.use("/", require("./routes/main"));
-  
+
   app.use(function (err, req, res, next) {
     if (err.code !== 'EBADCSRFTOKEN') return next(err)
 
@@ -63,37 +77,26 @@ global.antixsslinks = function(string = "") {
     res.send('form tampered with')
   })
   app.use(function (err, req, res, next) {
-
     if (err) {
       console.log(err);
       return res.status(500).send("Something happened: " + antixss(err));
     } else next();
   });
-  app.use("*", function(req, res) {
-    if(req.user) {
-      res.status(404).render("404", {
-        username: req.user.username,
-        csrfToken: req.csrfToken(),
-        logged: true,
-        antixss,
-        antixsslinks
+  app.use("*", function (req, res) {
+    res.status(404).render("404", {
+      username: req.user ? req.user.username : "stranger",
+      csrfToken: req.csrfToken(),
+      logged: Boolean(req.user),
+      antixss,
+      antixsslinks
     })
-    } else {
-      res.status(404).render("404", {
-        username: "stranger",
-        csrfToken: req.csrfToken(),
-        logged: false,
-        antixss,
-        antixsslinks
-    })
-    }
   });
   // listen for requests :)
   // In this case the requests will be local.
   const listener = app.listen(process.env.PORT, "127.0.0.1", () => {
     console.log("Your app is listening on port " + listener.address().port);
   });
-  if(process.argv[2] === "ci") {
+  if (process.argv[2] === "ci") {
     setTimeout(process.exit, 15000)
   }
 })().catch(err => {
